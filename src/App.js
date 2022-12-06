f68f4edbdf165eecbec382febc1982bb82a31aeb
Devin Raleigh <devinkr@gmail.com>
2022-12-06 10:55:18 -0700

    Add album info (title, artist, releaseDate, url)

--- src/App.js
+++ src/App.js
@@ -4,10 +4,13 @@ import albumsData from './top100.json';
 
 function App() {
 	const albums = albumsData.feed.entry.map((album) => ({
-		id: album.id.label,
+		id: album.id.attributes['im:id'],
 		name: album['im:name'].label,
 		artist: album['im:artist'].label,
 		img: album['im:image'][2].label,
+		price: album['im:price'],
+		releaseDate: new Date(album['im:releaseDate'].attributes.label),
+		url: album.link.attributes.href,
 	}));
 	return (
 		<>
--- src/components/AlbumCard.js
+++ src/components/AlbumCard.js
@@ -1,14 +1,38 @@
 import React from 'react';
 import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Link from '@mui/material/Link';
+
+const hoverBox = {
+	padding: '10px',
+	borderRadius: '10px',
+	'&:hover': {
+		backgroundColor: 'rgba(200, 200, 200, .5)',
+		transition: '.5s',
+	},
+};
 
 function AlbumCard({ album }) {
 	if (album) {
 		return (
-			<Box
-				component='img'
-				sx={{ backgroundColor: 'lightgreen' }}
-				src={album.img}
-				alt={album.name}></Box>
+			<Box component='div' display='flex'>
+				<Box
+					component='img'
+					sx={{ mr: 3, maxWidth: 140 }}
+					src={album.img}
+					alt={album.name}></Box>
+				<Box sx={{ height: '100%' }}>
+					<Typography variant='h6' component='h2'>
+						{album.name}
+					</Typography>
+					<Typography variant='body1'>
+						{album.artist} - {album.releaseDate.getFullYear()}
+					</Typography>
+					<Link href={album.url} sx={{ mt: 3 }}>
+						View on iTunes
+					</Link>
+				</Box>
+			</Box>
 		);
 	}
 }
