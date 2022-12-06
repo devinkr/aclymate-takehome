d2094c119eb517c78409023d6333dfe3876b1a91
Devin Raleigh <devinkr@gmail.com>
2022-12-06 10:26:43 -0700

    Add 3 column layout for lg screens

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -9,13 +9,20 @@ function AlbumList({ albums }) {
 	if (albums.length > 0) {
 		return (
 			<Container maxWidth='xl'>
-				<Typography variant='h2' sx={{ m: 8, textAlign: 'center' }}>
+				<Typography
+					variant='h2'
+					component='h1'
+					sx={{ m: 8, textAlign: 'center' }}>
 					iTunes Top100 Albums
 				</Typography>
 				<Box sx={{ width: '100%' }}>
-					<Grid container rowSpacing={3} columnSpacing={3}>
+					<Grid container rowSpacing={8} columnSpacing={8}>
 						{albums.map((album, index) => (
-							<Grid xs={12} md={6} key={`ALGrid-${index}`}>
+							<Grid
+								xs={12}
+								md={6}
+								lg={4}
+								key={`AlbumGrid-${index}`}>
 								<AlbumCard album={album} key={album.id} />
 							</Grid>
 						))}
